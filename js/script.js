7fe5f8147346f645b8bc375e44ffe26bdbf0264b
Ivan <ivan.s.palatov@gmail.com>
2020-04-21 11:46:59 +0400

    task09

---
+++ js/script.js
@@ -0,0 +1,33 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use sctrict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+
+    function HideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    HideTabContent(1);
+
+    function ShowTabContent(b) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+    }
+
+    info.addEventListener('click', function(e) {
+        if (e.target && e.target.classList.contains('info-header-tab')) {
+            for (let i=0; i < tab.length; i++ ) {
+                if (e.target == tab[i]) {
+                    HideTabContent(0);
+                    ShowTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
